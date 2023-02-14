1f348c6363c632500222d2832c16475ca82a9ddb
Sam Schams <samschams@gmail.com>
2023-02-14 19:23:55 +0100

    Mobile: Index page with layouts for hero, hours and services

---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				pageBackgroundColor: "white",
+				custom: {
+					pageBackground: {
+						white: "white",
+						blue: "#EEF7FC",
+						purple: "rgba(248,243,253,1)",
+					},
+					darkText: "#160623",
+					blue: {
+						light: "#CEEDFF",
+						medium: "#3CB7FC",
+						dark: "#1890CA",
+						darker: "#0872A6",
+					},
+					purple: {
+						light: "#DDC8F6",
+						dark: "#8C2ADA",
+					},
+				}
+			},
+			fontFamily: {
+				DIN: "'din-2014', 'Poppins', sans-serif",
+				Merriweather: "'Merriweather', serif",
+				Poppins: "'Poppins', 'din-2014', sans-serif"
+			}
+		},
+	},
+	plugins: [],
+}
